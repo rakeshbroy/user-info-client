b27d10c764c7c89dac7ebb967e9379a3436502a0
Rakesh Kumar <rkrakesh133@gmail.com>
2021-11-26 22:02:02 +0530

    Final commit

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,6 +7,8 @@ import * as _ from 'lodash';
 import { Subscription } from "rxjs";
 import { DataService } from 'src/app/services/data-service';
 import { AppEvents } from 'src/app/enum/events-enum';
+import { UrlsConfig } from 'src/app/config/urls-config';
+import axios from 'axios';
 
 @Component({
   selector: 'app-user',
@@ -16,15 +18,14 @@ import { AppEvents } from 'src/app/enum/events-enum';
 export class UserComponent implements OnInit {
 
   displayedColumns = ['id', 'firstName', 'lastName', 'email', 'mobile', 'actions'];
-  user: UserModel = {};
-  users: [UserModel] = [
-    {id: 1, firstName: 'Rakesh', lastName: 'Kumar', email: 'rkrakesh133@gmail.com', mobile: '9709374758'},
-  ]; 
+  user: any = {};
+  users: any; 
   public subscription!: Subscription;
 
   constructor(public toastr: ToastrService, public dialog: MatDialog, public dataService: DataService) { }
 
   ngOnInit(): void {
+    this.callApiToGetUsers();
     this.subscription = this.dataService.emitter.subscribe((event: AppEvents) => {
       if (event === AppEvents.AddUser) {
         this.callApiToAddUser()
@@ -60,18 +61,115 @@ export class UserComponent implements OnInit {
     this.openDialog('edit_user');
   }
 
+  callApiToGetUsers() {
+    axios.get(UrlsConfig.listUsers)
+    .then(result => {
+      if (!result.data.error) {
+        this.users = result.data.data.users;
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   callApiToAddUser() {
     console.log(this.user);
+    if (!this.isFormValid()) {
+      return;
+    }
+    const req = {
+      ...this.user
+    }
+    axios.post(UrlsConfig.addUser, req)
+    .then(result => {
+      if (!result.data.error) {
+        this.toastr.success('User added successfully');
+        this.callApiToGetUsers();
+        this.dialog.closeAll();
+      } else {
+        this.toastr.error(result.data.message);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
   callApiToDeleteUser() {
     console.log('deleting user');
   }
   callApiToEditUser() {
-    console.log(this.user);
+    if (!this.isFormValid()) {
+      return
+    }
+    const req = {
+      ...this.user
+    }
+    axios.post(UrlsConfig.UpdateUser, req)
+    .then(result => {
+      if (!result.data.error) {
+        this.toastr.success(result.data.message);
+        this.callApiToGetUsers();
+        this.dialog.closeAll();
+      } else {
+        this.toastr.error(result.data.message);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  isFormValid() {
+    if(_.isEmpty(this.user.firstName)) {
+      this.toastr.error('First name cant be empty');
+      return false;
+    }
+    if(_.isEmpty(this.user.lastName)) {
+      this.toastr.error('Last name cant be empty');
+      return false;
+    }
+    if(_.isEmpty(this.user.email)) {
+      this.toastr.error('Email cant be empty');
+      return false;
+    }
+    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.user.email)) {
+      this.toastr.error('Enter a valid email id');
+      return false;
+    }
+    if (!/[1-9][0-9]{9}/.test(this.user.mobile)) {
+      this.toastr.error('Enter 10 digit phone number');
+      return false;
+    }
+    if (this.user.password?.length < 8) {
+      this.toastr.error('Password must have min. 8 chars.');
+      return false;
+    }
+    if (!/[0-9]+/.test(this.user.password)) {
+      this.toastr.error('Password must have at least one number');
+      return false;
+    }
+    if (!/[A-Z]+/.test(this.user.password)) {
+      this.toastr.error('Password must have at least 1 uppercase character');
+      return false;
+    }
+    if (!/[!@#\$%\^\&*\)\(+=._-]+/.test(this.user.password)) {
+      this.toastr.error('Password must have at least 1 special character');
+      return false;
+    }
+    if (/\s+/.test(this.user.password)) {
+      this.toastr.error('Password Cannot contain a whitespace');
+      return false;
+    }
+    if (!_.isEqual(this.user.password, this.user.confirmPassword)) {
+      this.toastr.error('Password did not match to the confirm password');
+      return false;
+    }
+    return true;
   }
 
 }
---
+++ src/app/config/urls-config.ts
@@ -0,0 +1,7 @@
+const baseUrl = 'http://localhost:9004';
+
+export class UrlsConfig {
+    static addUser = baseUrl + '/user/add-user';
+    static listUsers = baseUrl + '/user/list-user';
+    static UpdateUser = baseUrl + '/user/edit-user';
+}
